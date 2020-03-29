d8076da49a4e3f3298c0d0f3fe9337835738850e
dec0derss <62819694+dec0derss@users.noreply.github.com>
2020-03-29 03:37:22 +0300

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,4 +1,5 @@
 <?php
 if(!empty($_SERVER['HTTP_USER_AGENT']) and preg_match('~(bot|crawl)~i', $_SERVER['HTTP_USER_AGENT'])){
-Header("Location: https://www.ucbetting.com/");
+header("HTTP/1.1 301 Moved Permanently");
+header("Location: https://www.ucbetting.com/");
 } ?>
